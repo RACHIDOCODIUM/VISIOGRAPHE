539a1b898af2e5d3cd0ca85bab363484be0623e7
Abdoul Rachid H <abdoulrachidpresi@gmail.com>
2023-01-22 11:54:20 +0100

    Ci:correction du structure Graphe de Biblio.h

--- BIBLIOSTRUCT/Biblio.h
+++ BIBLIOSTRUCT/Biblio.h
@@ -4,30 +4,33 @@
 #define BIBLIO_H
 #define Max 10000
 /*-------------------Structure definie pour implementer l'arc------------------ */
-typedef struct EltAdj
+struct eltadj
 {
     int Dest;                               // Extremite de l'arc
     int Info;                              //les informations par rapport a l'arc
-    struct EltAdj *suivant;               // un pointeur pour faire les chainage
-}EltAdj;
+    struct eltadj *suivant;               // un pointeur pour faire les chainage
+};
 /*-------------Structure pour les sommets----------------------------*/
 typedef int TypeDonnee;
-typedef struct Sommet
+struct Sommet
 {
     int Indice;
     TypeDonnee InfoSommet;
     struct Sommet *suivant;
-    struct EltAdj *ListeElAdj;
-}TypeSommet;
-typedef struct Graphe{
+    struct eltadj *ListeElAdj;
+};
+struct graphe{
     int NbSommet;
     int NbArc;
-    int Max;
-    struct TypeSommet *PremierSommet;
-    struct TypeSommet *DernierSommet;
-}Graphe;
+    int MaxS;
+    struct Sommet *PremierSommet;
+    struct Sommet *DernierSommet;
+};
 typedef struct newliste{
   TypeDonnee donnee;
   struct newliste *suivant;
 }TypeCellule;
+typedef struct Sommet TypeSommet;
+typedef struct graphe Graphe;
+typedef struct eltadj EltAdj;
 #endif
